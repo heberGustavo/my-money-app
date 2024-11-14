f529842facfb58008bfd4e0bbb66f65eea5a2738
heberGustavo <heber.gbarbosa@hotmail.com>
2024-11-14 07:55:27 -0300

    Create Footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2024 - Heber Gustavo
+        </strong>
+    </footer>
+)
